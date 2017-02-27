07460e5781afa6b9a9f526c19778932933589940
Mohammed Sohail <sohailsameja@gmail.com>
2017-02-27 17:18:18 +0530

    [Sol] 1

--- 2017/Country Leader/index.js
+++ 2017/Country Leader/index.js
@@ -1,35 +1,25 @@
-var _ = require('lodash')
-var fs = require('fs')
+const _ = require('lodash');
+const fs = require('fs');
 
 fs.readFile('./input/' + process.argv[2] + '.in', 'utf8', (err, data) => {
     if (err) {
         return console.log(err);
     }
-    run(data);
+    let solution = run(data)
+    _.each(solution, (element, index) => {
+        let output = `Case #${(index + 1)}: ${element}`
+        console.log(output)
+    })
 });
 
 function run(input) {
-    var lines = input.split('\n')
-    var testCases = lines.shift();
-    var obj = {};
-    for (var i = 0; i < testCases; i++) {
-        var testCase = obj[i + 1] = [];
-        var numberOfNames = lines.shift();
-        for (var j = 0; j < numberOfNames; j += 1) {
-            let candidateNames = lines.shift()
-            let strippedNames = _.uniq(candidateNames).join('')
-            testCase.push(strippedNames)
-        }
-    }
-    for (var k = 0; k < testCases; k++) {
-        console.log(`Case #${[k + 1]}: ${findLongestName(obj[k + 1])}`)
-        
-    }
-}
-
-function findLongestName(str) {
-  var longestWord = str.reduce(function(longest, currentWord) {
-    return currentWord.length > longest.length ? currentWord : longest;
-  }, '');
-  return longestWord;
+  var lines = input.split('\n');
+  return _.times(lines.shift(), () => {
+    return _(lines)
+      .splice(0, lines.shift())
+      .map(item => [_(item).split('').uniq().size(), item])
+      .orderBy([0, 1], ['asc', 'desc'])
+      .map(1)
+      .last();
+  });
 }
